44cf9b590942fe62ac3311188b2e09b494fb426b
Mah <for.my.git.work@gmail.com>
2022-01-15 22:34:50 +0100

    Add Button, useState

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  const [text, setText] = useState("Welcome! If you are ready please confirm");
   return (
     <View style={styles.container}>
-      <Text>edit Open up App.js to start working on your app!</Text>
+      <Text>{text}</Text>
+      <Button title="confirm" onPress={() => setText("Greate!")} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
